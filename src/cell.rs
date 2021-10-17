11a2cc1491fdd50bf66bdb22c7df88e96ba837ce
Alexander Nookinov <anookinov@gmail.com>
2021-10-17 20:49:07 +0700

    create refcell

--- src/cell.rs
+++ src/cell.rs
@@ -9,7 +9,7 @@ pub struct Cell<T> {
 // unsafe impl<T> Sync for Cell<T> {}
 
 impl<T> Cell<T> {
-    fn new(value: T) -> Self {
+    pub fn new(value: T) -> Self {
         Cell {
             value: UnsafeCell::new(value),
         }
@@ -31,38 +31,38 @@ impl<T> Cell<T> {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::Cell;
+// #[cfg(test)]
+// mod test {
+//     use super::Cell;
 
-    // #[test]
-    // fn bad() {
-    //     use std::sync::Arc;
-    //     let x = Arc::new(Cell::new(0));
-    //     let x1 = Arc::clone(&x);
-    //     let jh1 = std::thread::spawn(move || {
-    //         for _ in 0..1000000 {
-    //             let x = x1.get();
-    //             x1.set(x + 1);
-    //         }
-    //     });
-    //     let x2 = Arc::clone(&x);
-    //     let jh2 = std::thread::spawn(move || {
-    //         for _ in 0..1000000 {
-    //             let x = x2.get();
-    //             x2.set(x + 1);
-    //         }
-    //     });
-    //     jh1.join().unwrap();
-    //     jh2.join().unwrap();
-    //     assert_eq!(x.get(), 2000000);
-    // }
+//     #[test]
+//     fn bad() {
+//         use std::sync::Arc;
+//         let x = Arc::new(Cell::new(0));
+//         let x1 = Arc::clone(&x);
+//         let jh1 = std::thread::spawn(move || {
+//             for _ in 0..1000000 {
+//                 let x = x1.get();
+//                 x1.set(x + 1);
+//             }
+//         });
+//         let x2 = Arc::clone(&x);
+//         let jh2 = std::thread::spawn(move || {
+//             for _ in 0..1000000 {
+//                 let x = x2.get();
+//                 x2.set(x + 1);
+//             }
+//         });
+//         jh1.join().unwrap();
+//         jh2.join().unwrap();
+//         assert_eq!(x.get(), 2000000);
+//     }
 
-    // #[test]
-    // fn bad2() {
-    //     let x = Cell::new(String::from("hello"));
-    //     let first = x.get();
-    //     x.set(String::new());
-    //     eprintln!("{}", first);
-    // }
-}
+//     #[test]
+//     fn bad2() {
+//         let x = Cell::new(String::from("hello"));
+//         let first = x.get();
+//         x.set(String::new());
+//         eprintln!("{}", first);
+//     }
+// }
